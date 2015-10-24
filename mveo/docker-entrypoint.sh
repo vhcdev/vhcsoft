a05a520beec0a1e973b35c94cec4407095fdf876
chu viet hung <viethungde@gmail.com>
2015-10-24 17:11:21 +0200

    update dockerfile

--- mveo/docker-entrypoint.sh
+++ mveo/docker-entrypoint.sh
@@ -3,11 +3,11 @@
 echo "Starting the application... "
 CMD="java -jar app.jar --logging.level.org.springframework.web=ERROR"
 
-if [ -n "$MVEODB_PORT_3306_TCP_ADDR" ]
-then
-	echo "mysql host: $MVEODB_PORT_3306_TCP_ADDR"
-	CMD="java -jar app.jar --logging.level.org.springframework.web=ERROR --spring.datasource.url=jdbc:mysql://$MVEODB_PORT_3306_TCP_ADDR:3306/mveo"
-fi
+#if [ -n "$MVEODB_PORT_3306_TCP_ADDR" ]
+#then
+#	echo "mysql host: $MVEODB_PORT_3306_TCP_ADDR"
+#	CMD="java -jar app.jar --logging.level.org.springframework.web=ERROR --spring.datasource.url=jdbc:mysql://$MVEODB_PORT_3306_TCP_ADDR:3306/mveo"
+#fi
 
 eval $CMD
 
